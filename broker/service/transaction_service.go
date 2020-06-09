e66cbe92c80f1ad35a7f938bef1b150c1235a920
Elon Choi  (Elon) <elon.choi@paust.io>
2020-06-10 03:11:00 +0900

    Refactor transaction_service and add other message handles

--- broker/service/transaction_service.go
+++ broker/service/transaction_service.go
@@ -54,14 +54,19 @@ func (service *TransactionService) registerMessageHandles() (*message.Handler, <
 	msgHandler := &message.Handler{}
 	handleErrCh := make(chan error)
 
-	msgHandler.RegisterMsgHandle(&paustqproto.CreateTopicRequest{}, func(msg proto.Message, args ...interface{}) {
+	handleSessionMsg := func(responseMsg proto.Message, args ...interface{}) {
+		if len(args) == 0 {
+			handleErrCh <- pqerror.UnhandledError{ErrStr: "session argument required"}
+			return
+		}
+
 		session, ok := args[0].(*internals.Session)
 		if !ok {
 			handleErrCh <- pqerror.UnhandledError{ErrStr: "session argument required"}
 			return
 		}
-		res := service.apiService.CreateTopic(msg.(*paustqproto.CreateTopicRequest))
-		qMsg, err := message.NewQMessageFromMsg(res)
+
+		qMsg, err := message.NewQMessageFromMsg(responseMsg)
 
 		if err != nil {
 			handleErrCh <- err
@@ -70,24 +75,26 @@ func (service *TransactionService) registerMessageHandles() (*message.Handler, <
 		if err := session.Write(qMsg); err != nil {
 			handleErrCh <- err
 		}
+	}
+
+	msgHandler.RegisterMsgHandle(&paustqproto.CreateTopicRequest{}, func(msg proto.Message, args ...interface{}) {
+		handleSessionMsg(service.apiService.CreateTopic(msg.(*paustqproto.CreateTopicRequest)), args...)
 	})
 
 	msgHandler.RegisterMsgHandle(&paustqproto.DeleteTopicRequest{}, func(msg proto.Message, args ...interface{}) {
-		session, ok := args[0].(*internals.Session)
-		if !ok {
-			handleErrCh <- pqerror.UnhandledError{ErrStr: "session argument required"}
-			return
-		}
-		res := service.apiService.DeleteTopic(msg.(*paustqproto.DeleteTopicRequest))
-		qMsg, err := message.NewQMessageFromMsg(res)
+		handleSessionMsg(service.apiService.DeleteTopic(msg.(*paustqproto.DeleteTopicRequest)), args...)
+	})
 
-		if err != nil {
-			handleErrCh <- err
-			return
-		}
-		if err := session.Write(qMsg); err != nil {
-			handleErrCh <- err
-		}
+	msgHandler.RegisterMsgHandle(&paustqproto.ListTopicRequest{}, func(msg proto.Message, args ...interface{}) {
+		handleSessionMsg(service.apiService.ListTopic(msg.(*paustqproto.ListTopicRequest)), args...)
+	})
+
+	msgHandler.RegisterMsgHandle(&paustqproto.DescribeTopicRequest{}, func(msg proto.Message, args ...interface{}) {
+		handleSessionMsg(service.apiService.DescribeTopic(msg.(*paustqproto.DescribeTopicRequest)), args...)
+	})
+
+	msgHandler.RegisterMsgHandle(&paustqproto.Ping{}, func(msg proto.Message, args ...interface{}) {
+		handleSessionMsg(service.apiService.Heartbeat(msg.(*paustqproto.Ping)), args...)
 	})
 
 	return msgHandler, handleErrCh
