b20ed752d4add1e1d4f28e9e201e5461fc809340
Elon Choi  (Elon) <elon.choi@paust.io>
2020-06-11 15:13:46 +0900

    Add BROKER_TOPIC path for zkclient and update zkClient_test

--- zookeeper/zk_client_test.go
+++ zookeeper/zk_client_test.go
@@ -1,8 +1,10 @@
 package zookeeper
 
 import (
+	"errors"
 	"fmt"
 	"github.com/paust-team/paustq/broker/internals"
+	"github.com/paust-team/paustq/pqerror"
 	"os"
 	"testing"
 )
@@ -34,18 +36,21 @@ func TestMain(m *testing.M) {
 func TestZKClient_AddBroker(t *testing.T) {
 	host, err := GetOutboundIP()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if err = zkClient.AddBroker(host.String()); err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	brokers, err := zkClient.GetBrokers()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
+	if len(brokers) == 0 {
+		t.Fatal("no brokers")
+	}
 	if brokers[0] != host.String() {
 		t.Error("failed to add broker ", host.String())
 	}
@@ -60,33 +65,36 @@ func TestZKClient_AddTopic(t *testing.T) {
 	topicValue := internals.NewTopicValueFromValues(expectedTopicMeta, expectedNumPartitions, expectedReplicationFactor)
 	err := zkClient.AddTopic(expectedTopic, topicValue)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	topics, err := zkClient.GetTopics()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
+	}
+	if len(topics) == 0 {
+		t.Fatal("no topics")
 	}
 
 	if topics[len(topics)-1] != expectedTopic {
-		t.Error("failed to add topic ", expectedTopic)
+		t.Fatal("failed to add topic", expectedTopic,topics[len(topics)-1])
 	}
 
 	targetTopicValue, err := zkClient.GetTopic(expectedTopic)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if targetTopicValue.TopicMeta() != expectedTopicMeta {
-		t.Error("topic meta not matched ", expectedTopicMeta, targetTopicValue.TopicMeta())
+		t.Fatal("topic meta not matched ", expectedTopicMeta, targetTopicValue.TopicMeta())
 	}
 
 	if targetTopicValue.NumPartitions() != expectedNumPartitions {
-		t.Error("num partition not matched ", expectedNumPartitions, targetTopicValue.NumPartitions())
+		t.Fatal("num partition not matched ", expectedNumPartitions, targetTopicValue.NumPartitions())
 	}
 
 	if targetTopicValue.ReplicationFactor() != expectedReplicationFactor {
-		t.Error("replication factor not matched ", expectedReplicationFactor, targetTopicValue.ReplicationFactor())
+		t.Fatal("replication factor not matched ", expectedReplicationFactor, targetTopicValue.ReplicationFactor())
 	}
 
 }
@@ -100,35 +108,37 @@ func TestZKClient_AddTopicBroker(t *testing.T) {
 	topicValue := internals.NewTopicValueFromValues(expectedTopicMeta, expectedNumPartitions, expectedReplicationFactor)
 	err := zkClient.AddTopic(expectedTopic, topicValue)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	topics, err := zkClient.GetTopics()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
-
+	if len(topics) == 0 {
+		t.Fatal("no topics")
+	}
 	if topics[len(topics)-1] != expectedTopic {
-		t.Error("failed to add topic ", expectedTopic)
+		t.Fatal("failed to add topic ", expectedTopic)
 	}
 
 	host, err := GetOutboundIP()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	err = zkClient.AddTopicBroker(expectedTopic, host.String())
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	brokers, err := zkClient.GetTopicBrokers(expectedTopic)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if brokers[len(brokers)-1] != host.String() {
-		t.Error("failed to add topic broker ", host)
+		t.Fatal("failed to add topic broker ", host)
 	}
 }
 
@@ -141,22 +151,22 @@ func TestZKClient_RemoveTopic(t *testing.T) {
 	topicValue := internals.NewTopicValueFromValues(expectedTopicMeta, expectedNumPartitions, expectedReplicationFactor)
 	err := zkClient.AddTopic(topic, topicValue)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	err = zkClient.RemoveTopic(topic)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	topics, err := zkClient.GetTopics()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	for _, top := range topics {
 		if top == topic {
-			t.Errorf("topic did not deleted")
+			t.Error("topic did not deleted")
 		}
 	}
 }
@@ -165,19 +175,19 @@ func TestZKClient_RemoveBroker(t *testing.T) {
 	broker := "127.0.0.1"
 	err := zkClient.AddBroker(broker)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	err = zkClient.RemoveBroker(broker)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	brokers, err := zkClient.GetBrokers()
 
 	for _, b := range brokers {
 		if b == broker {
-			t.Errorf("broker did not deleted")
+			t.Error("broker did not deleted")
 		}
 	}
 }
@@ -191,41 +201,44 @@ func TestZKClient_RemoveTopicBroker(t *testing.T) {
 	topicValue := internals.NewTopicValueFromValues(expectedTopicMeta, expectedNumPartitions, expectedReplicationFactor)
 	err := zkClient.AddTopic(topic, topicValue)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	topics, err := zkClient.GetTopics()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if topics[len(topics)-1] != topic {
-		t.Error("failed to add topic ", topic)
+		t.Fatal("failed to add topic ", topic)
 	}
 
 	host, err := GetOutboundIP()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	err = zkClient.AddTopicBroker(topic, host.String())
 	if err != nil {
-		t.Error(err)
+		var e pqerror.ZKTargetAlreadyExistsError
+		if !errors.As(err, &e) {
+			t.Fatal(err)
+		}
 	}
 
 	err = zkClient.RemoveTopicBroker(topic, host.String())
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	brokers, err := zkClient.GetTopicBrokers(topic)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	for _, broker := range brokers {
 		if broker == host.String() {
-			t.Errorf("topic broker did not deleted")
+			t.Error("topic broker did not deleted")
 		}
 	}
 }
