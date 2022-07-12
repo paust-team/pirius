9fb6258bc85e8bf6621b83d649b451841a82be3f
1dennispark <dennis.park@paust.io>
2022-07-12 17:27:51 +0900

    Support to running on the docker into eks

--- start-shapleq.sh
+++ start-shapleq.sh
@@ -2,17 +2,7 @@
 
 set -u
 
-zkhost=$1
-zkport=$2
-configpath=$3
-res=$( echo ruok | nc $zkhost $zkport )
-
-
-while [ "$res" != "imok" ]
-do
-  echo "waiting for zookeeper run" 
-  sleep 1
-  res=$(echo ruok | nc $zkhost $zkport)
-done
+zkquorum=$1
+configpath=$2
 sleep 3
-/go/bin/shapleq start --zk-host "$zkhost" --zk-port "$zkport" -i "$configpath"
+bin/shapleq start --zk-quorum "$zkquorum" -i "$configpath"
