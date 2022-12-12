f295e284c916f19e03fc086d93f3705b690cf125
Elon Choi  (Elon) <elon.choi@paust.io>
2022-12-12 10:02:24 +0900

    Add broker package and implement topic rpc

--- bootstrapping/path/release.go
+++ bootstrapping/path/release.go
@@ -6,7 +6,6 @@ const (
 	ShapleQPath    = "/qdata"
 	BrokersPath    = "/qdata/brokers"
 	TopicsPath     = "/qdata/topics"
-	TopicsLockPath = "/qdata/topics-lock"
 	LockPath       = "/qdata/lock"
 	TopicsLockPath = "/qdata/lock/topics"
 )
---
+++ broker/cmd/cli/cli.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func checkRunningBrokerProcess() (bool, int) {
+
+	parentPid := os.Getppid()
+	existsProcess := "ps -ef | grep 'qbroker start' | grep -v grep | awk '{print $2, $3}'"
+	out, err := exec.Command("bash", "-c", existsProcess).Output()
+	if err != nil {
+		return false, 0
+	}
+	outs := strings.Split(string(out), "\n")
+	for _, ln := range outs {
+		if len(ln) != 0 && !strings.Contains(ln, strconv.Itoa(parentPid)) {
+			pid, err := strconv.Atoi(strings.Split(ln, " ")[0])
+			if err != nil {
+				return false, 0
+			}
+			return true, pid
+		}
+	}
+	return false, 0
+}
+
+var brokerCmd = &cobra.Command{
+	Use:   "qbroker [command] (flags)",
+	Short: "ShapleQ Broker cli",
+}
+
+func Main() {
+
+	brokerCmd.AddCommand(
+		NewStartCmd(),
+	)
+
+	if err := brokerCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
