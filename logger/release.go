d4812737574e7b960cf23a6b1255a3ad89fab9a3
Elon Choi  (Elon) <elon.choi@paust.io>
2022-12-28 11:36:44 +0900

    test

--- logger/release.go
+++ logger/release.go
@@ -4,22 +4,26 @@ package logger
 
 import (
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 var log *zap.Logger
 
 func init() {
+	//var err error
+	//config := zap.NewProductionConfig()
+	//encoderConfig := zap.NewProductionEncoderConfig()
+	//encoderConfig.TimeKey = "timestamp"
+	//encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	//encoderConfig.StacktraceKey = ""
+	//config.Encoding = "console"
+	//config.EncoderConfig = encoderConfig
+	//
+	//log, err = config.Build(zap.AddCallerSkip(1), zap.AddStacktrace(zap.ErrorLevel))
+	//if err != nil {
+	//	panic(err)
+	//}
 	var err error
-	config := zap.NewProductionConfig()
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.TimeKey = "timestamp"
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	encoderConfig.StacktraceKey = ""
-	config.Encoding = "console"
-	config.EncoderConfig = encoderConfig
-
-	log, err = config.Build(zap.AddCallerSkip(1), zap.AddStacktrace(zap.ErrorLevel))
+	log, err = zap.NewDevelopment(zap.AddStacktrace(zap.ErrorLevel))
 	if err != nil {
 		panic(err)
 	}
